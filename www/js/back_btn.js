15c6058146a26350af95ac302a74f712db9563c6
shakibania <shakiba@gmail.com>
2017-05-05 22:26:57 +0430

    0

--- www/js/back_btn.js
+++ www/js/back_btn.js
@@ -63,19 +63,6 @@
 			}
 			else
 			{
-				var options_back = {
-				  "direction"		: "left", // "up", // 'left|right|up|down', default 'left' (which is like 'next')
-				  "duration"		:  700, // in milliseconds (ms), default 400
-				  "iosdelay"		:  110, // ms to wait for the iOS webview to update before animation kicks in, default 60
-				  "androiddelay"	:  110, // same as above but for Android, default 70
-				  "href"			: href //"adab.html"
-				};
-				window.plugins.nativepagetransitions.slide(
-				  options_back,
-				  function (msg) {console.log("success: " + msg)}, // called when the animation has finished
-				  function (msg) {console.log("error: " + msg)} // called in case you pass in weird values
-				);
-				/*
 				var options = {
 				  "duration"       :  700, // in milliseconds (ms), default 400
 				  "iosdelay"       :  100, // ms to wait for the iOS webview to update before animation kicks in, default 60
@@ -87,7 +74,6 @@
 				  function (msg) {console.log("success: " + msg)}, // called when the animation has finished
 				  function (msg) {alert("error: " + msg)} // called in case you pass in weird values
 				);
-				*/
 			}
 		} else {
 			window.location.href = href;
